95c683b553d6214dec84d6132b225102a3a5b6a1
Wojciech T <hadwao2@gmail.com>
2018-09-14 17:00:30 +0200

    cars (printf)

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,12 @@
 import java.time.YearMonth;
 
-public class book {
+public class Book {
     String title;
     String author;
     int pageAm;
     int yearRealise;
 
-    book (String T, String A, int P, int R){
+    Book(String T, String A, int P, int R){
         title = T;
         author = A;
         pageAm = P;
--- src/Cars.java
+++ src/Cars.java
@@ -1,17 +1,9 @@
-public class cars {
+public class Cars {
     public static void main(String[] args) {
         Car car1 = new Car("BMW", "M5", 2007, 2.6); //marka, model, rocznik, pojemność silnika
         Car car2 = new Car("Opel", "Vectra", 2004, 1.8);
 
-
         System.out.printf("Samochód 1:\n Marka: %s, model: %s, rocznik: %d, pojemność: %.1f \n", car1.brand, car1.model, car1.year, car1.capacity );
         System.out.printf("Samochód 2:\n Marka: %s, model: %s, rocznik: %d, pojemność: %.1f ", car2.brand, car2.model, car2.year, car2.capacity );
-
-
-
-
-
     }
-
-
 }
